c1c70936be955c138a63f8098dc4cfde38fe6715
𝓥𝓮𝓲𝓵 <2054575@learn.vsb.bc.ca>
2026-09-15 21:47:04 -0700

    4.6

--- MultAdd.java
+++ MultAdd.java
@@ -4,7 +4,7 @@ public static double multadd(double a, double b, double c){
 	
 	}
 	
-	public static void main(String[] args){
+public static void main(String[] args){
 		
 		double simpleAdd=multadd(1.0,2.0,3.0);
 		double trigResult=multadd(0.5,Math.cos(0.25*Math.PI),Math.sin(0.25*Math.PI));
