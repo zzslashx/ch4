db2bf66ec7ffd4778a57a03c5ce1822fe6c1de5d
𝓥𝓮𝓲𝓵 <2054575@learn.vsb.bc.ca>
2026-09-22 14:44:04 -0700

    Update FormatDate.java

--- FormatDate.java
+++ FormatDate.java
@@ -1,3 +1,4 @@
+public class FormatDate{
 public static void printAmerican(String day, int date, String month, int year){
 	
 	System.out.println(day+", " + month + " " + date + ", " +year);
@@ -15,4 +16,5 @@ public static void main(String[] args){
 	printAmerican("Thursday",12,"August",2012);
 	printEuropean("Thursday",12,"August",2012);
 	
+}
 }
